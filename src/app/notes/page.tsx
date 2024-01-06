cbd8ce62493270a98dcad7fb0c26c3aadfec000b
Kovács Erik <kovi.erik98@gmail.com>
2024-01-06 22:45:35 +0100

    adding metadata to home/notes/sign pages

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next";
 import React from "react";
 
 type Props = {};
+
+export const metadata: Metadata = {
+  title: "NoteBrain - Notes",
+};
 
 const NotesPage = (props: Props) => {
   return <div>Here will be your notes!</div>;
